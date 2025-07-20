a75455b14d95140c758abae7268d00042d2e76c8
Nthabiseng <molekwanthabiseng@gmail.com>
2025-07-20 11:59:18 +0200

    Added info

---
+++ my-company/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div style={{ fontFamily: 'Arial, sans-serif' }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ my-company/src/components/App.jsx
@@ -0,0 +1,20 @@
+import UserProfile from './UserProfile';
+import WelcomeMessage from './WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+
+
+const App = () => {
+    return (
+        <div>
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+            <WelcomeMessage Hello everyone I am learning React at ALX /> 
+            <Header My Favorite Cities />
+            <MainContent I love to visit New York Paris and Tokyo />
+            <Footer City Lovers />
+        </div>
+    );
+};
+
+export default App;
