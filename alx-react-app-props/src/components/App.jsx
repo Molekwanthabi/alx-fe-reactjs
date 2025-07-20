c9de89186860433a8c18b03e214fcbdb481a30fa
Nthabiseng Molekwa <molekwanthabiseng@gmail.com>
2025-07-20 13:39:01 +0200

    Update App.jsx

--- alx-react-app-props/src/components/App.jsx
+++ alx-react-app-props/src/components/App.jsx
@@ -1,10 +1,14 @@
 import ProfilePage from './ProfilePage';
-import {UserContext} from './Usercontext';
+import UserContext from './UserContext'; 
 
 function App() {
-    const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-    return <ProfilePage userData={userData} />;
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
 
 export default App;
