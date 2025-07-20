640da12e843813b8389005b1c542e88b4912ed61
Nthabiseng Molekwa <molekwanthabiseng@gmail.com>
2025-07-20 16:43:58 +0200

    Add files via upload

---
+++ my-company/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './Navbar';
+import Home from './Home';
+import About from './About';
+import Services from './Services';
+import Contact from './Contact';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div style={{ fontFamily: 'Arial, sans-serif' }}>
+        <Navbar />
+        <Contact />
+        <Home />
+        <About />
+        <Services />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
