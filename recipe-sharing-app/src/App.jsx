f744010ce57b1e141311dd02bf541bd666f238a2
Nthabiseng Molekwa <molekwanthabiseng@gmail.com>
2025-07-23 02:35:31 +0200

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -6,10 +6,11 @@ import RecommendationsList from './components/RecommendationsList';
 
 function App() {
   return (
-    <div>
+    <div style={{ padding: '2rem' }}>
       <h1>🍲 Recipe Sharing App</h1>
       <AddRecipeForm />
       <RecipeList />
+      <hr />
       <FavoritesList />
       <RecommendationsList />
     </div>
