994063bfc110400585211d620cc35ad718cadf4d
Nthabiseng Molekwa <molekwanthabiseng@gmail.com>
2025-07-23 02:16:00 +0200

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,24 +1,9 @@
-// RecipeList component
+  import { useState } from 'react';
   import { useRecipeStore } from './recipeStore';
 
   const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
 
-    return (
-      <div>
-        {recipes.map(recipe => (
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))}
-      </div>
-    );
-  };
-
-  // AddRecipeForm component
-  import { useState } from 'react';
-  import { useRecipeStore } from './recipeStore';
 
   const AddRecipeForm = () => {
     const addRecipe = useRecipeStore(state => state.addRecipe);
@@ -33,6 +18,15 @@
     };
 
     return (
+   <div>
+        {recipes.map(recipe => (
+          <div key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))}
+      </div>
+      
       <form onSubmit={handleSubmit}>
         <input
           type="text"
