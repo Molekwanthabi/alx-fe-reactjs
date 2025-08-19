7d9e9b16c29a07b2f94b5d6d0dd4e73fa9252da2
Nthabiseng Molekwa <molekwanthabiseng@gmail.com>
2025-08-19 17:20:19 +0200

    Update Profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,1 +1,24 @@
+import { Routes, Route, Link, Outlet } from 'react-router-dom';
+import ProfileDetails from '../components/ProfileDetails';
+import ProfileSettings from '../components/ProfileSettings';
 
+function Profile() {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <nav className="flex gap-4 mb-4">
+        <Link to="details">Profile Details</Link>
+        <Link to="settings">Profile Settings</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<ProfileDetails />} /> {/* Default nested route */}
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+      <Outlet /> {/* Render child routes */}
+    </div>
+  );
+}
+
+export default Profile;
